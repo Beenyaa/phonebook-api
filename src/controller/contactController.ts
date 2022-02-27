a7cd440c763ced65dae756c05ea8999d08cc3017
Beenyaa <bencegadanyi1@hotmail.com>
2022-02-27 20:13:32 +0000

    Bug Fix:  Get /contact path returns all Contacts

--- src/controller/contactController.ts
+++ src/controller/contactController.ts
@@ -10,7 +10,7 @@ class ContactController {
   public async listAll(req: Request, res: Response): Promise<Response> {
     let contacts;
     try {
-      contacts = await getRepository(Contact).findOneOrFail(req.params.id);
+      contacts = await getRepository(Contact).findOne();
     } catch (error) {
       //If not found, send a 404 response
       return res.status(404).send("No contacts found");
